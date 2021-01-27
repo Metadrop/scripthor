e44a66300e6f4d6a7049a17085731ad0bd32a35d
Eduardo Morales <eduardo.morales@metadrop.net>
2021-01-27 19:43:49 +0100

    Show project url on traefik setup.

--- bin/setup-traefik-port.sh
+++ bin/setup-traefik-port.sh
@@ -4,3 +4,4 @@
 docker-compose up -d traefik
 # Setup the port on .env file.
 sed -i "s/CONTAINER_PORT.*/CONTAINER_PORT\=$(docker-compose port traefik 80 | cut -d: -f2)/g" .env
+echo "Project URL: http://$(grep -oP '(?<=PROJECT_BASE_URL=).*' .env):$(docker-compose port traefik 80 | cut -d: -f2)"
