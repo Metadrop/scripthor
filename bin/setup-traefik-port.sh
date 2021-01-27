db56c93268e7696ede89b489825d4e6a76711dfd
Eduardo Morales <eduardo.morales@metadrop.net>
2021-01-27 18:27:07 +0100

    Remove semicolon

--- bin/setup-traefik-port.sh
+++ bin/setup-traefik-port.sh
@@ -3,4 +3,4 @@
 # Star traefik to generate random port.
 docker-compose up -d traefik
 # Setup the port on .env file.
-sed -i "s/CONTAINER_PORT.*/CONTAINER_PORT\=$(docker-compose port traefik 80 | cut -d: -f2)/g" .env'
+sed -i "s/CONTAINER_PORT.*/CONTAINER_PORT\=$(docker-compose port traefik 80 | cut -d: -f2)/g" .env
