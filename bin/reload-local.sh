09a7b8853018ea7e9c622b34245c122738efec3d
David López <david.lopez@metadrop.net>
2021-02-25 09:20:59 +0100

    Drupal updb and drush cim where duplicated

--- bin/reload-local.sh
+++ bin/reload-local.sh
@@ -251,10 +251,6 @@ then
   # Execute updates and import configuration.
   $DOCKER_EXEC_PHP drush @${LOCAL_ALIAS} updb -y
 
-  $DOCKER_EXEC_PHP drush @${LOCAL_ALIAS} cim sync -y
-
-  $DOCKER_EXEC_PHP drush @${LOCAL_ALIAS} updb -y
-
   if [[ ${SKIP_TRANSLATIONS} = false ]]
   then
     $DOCKER_EXEC_PHP drush @${LOCAL_ALIAS} locale-check
