d6cffc23c72f4b766df710d5505d9d0d8eaa5469
jorgetutor <jorge.tutor@metadrop.net>
2022-04-18 13:36:09 +0200

    SOLR - simplify cp script

--- bin/solr-sync.sh
+++ bin/solr-sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 PROJECT_NAME=$1
-SOLR_CONTAINER=${2:- 'solr'}
-CORES=${3:- 'default'}
+SOLR_CONTAINER=${2:-'solr'}
+CORES=${3:-'default'}
 for core in ${CORES}
 do
-  docker cp solr/cores/ $(docker ps --filter name="^${PROJECT_NAME}_solr" --format "{{ .ID }}"):"/tmp/cores"
+  docker cp solr/cores/ "${PROJECT_NAME}_${SOLR_CONTAINER}:/tmp/cores"
   docker-compose exec -T ${SOLR_CONTAINER} sh -c "solr delete -c $core" || true
   docker-compose exec -T ${SOLR_CONTAINER} sh -c "solr create_core -c $core -d /tmp/cores/$core"
 done
