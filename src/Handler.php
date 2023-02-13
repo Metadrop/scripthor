3db758490f0be2f78826d84262e22e5f6c871c96
Omar <omar.lopesino@metadrop.net>
2023-02-13 16:23:18 +0100

    Prevent database to not be present

--- src/Handler.php
+++ src/Handler.php
@@ -230,6 +230,9 @@ class Handler {
    */
   protected function installDrupal($project_name) {
     if ($this->io->askConfirmation('Do you want to install Drupal? (Y/n) ')) {
+      // Wait 10 seconds to prevent the database to not be present.
+      $this->io->write("Waiting for database to be ready:\n");
+      sleep(10);
       copy('./web/sites/default/example.settings.local.php', './web/sites/default/settings.local.php');
       $drush_yml = file_get_contents('./web/sites/default/example.local.drush.yml');
       $drush_yml = str_replace('example', $project_name, $drush_yml);
